734dad6a7220705a4408dce6ecaa3c1f70be2cd6
Vivian Lim <vivvnlim@gmail.com>
2017-08-29 19:53:13 -0700

    Allow for multiple cores per rom type by mapping core name to rom format

--- py_retro/game_info_reader.py
+++ py_retro/game_info_reader.py
@@ -2,14 +2,24 @@ import ctypes
 module_name = "get_game_info"
 
 class GameInfoReader:
-    def get_gambatte_game_info(self, data):
+    coreToRomType = {
+        'gambatte': 'gbc'
+    }
+
+    def get_gbc_game_info(self, data):
         return {
             'name': ctypes.string_at(data[0x134:0x143])
         }
 
     def get_info(self, romData, coreName):
-        getInfoForCore = getattr(self, "get_{}_game_info".format(coreName.lower()))
-        if getInfoForCore == None:
-            print("Couldn't get game info since the core '{}' is unknown.".format(coreName))
+        romType = self.coreToRomType.get(coreName.lower())
+        if romType == None:
+            print("Couldn't get game info because the core '{}' is unknown.".format(coreName))
             return {}
+
+        getInfoForCore = getattr(self, "get_{}_game_info".format(romType))
+        if getInfoForCore == None:
+            print("Couldn't get game info since the rom type '{}' is unknown.".format(romType))
+            return {}
+
         return getInfoForCore(romData)
