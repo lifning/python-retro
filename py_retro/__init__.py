4146e010d24cae67984d6108ad4aa5606b4eb0ff
lifning <definelightning@gmail.com>
2017-09-06 22:57:40 -0700

    Fixes for audio callbacks
    - Restore pygame_audio as an option, and rewrite it to use mixer.Channel.queue()
    - Fix missing factor of two in buffer copies (should've been "number of
      frames * 2 bytes * 2 stereo channels")
    - Add a hack to portaudio driver to adjust playback frequency if it's too slow

--- py_retro/__init__.py
+++ py_retro/__init__.py
@@ -3,6 +3,7 @@ from . import core
 from . import pygame_video
 
 from . import portaudio_audio
+from . import pygame_audio
 from . import wave_audio
 
 from . import pygame_input
--- py_retro/portaudio_audio.py
+++ py_retro/portaudio_audio.py
@@ -12,39 +12,59 @@ from .retro_globals import HACK_need_audio_sample_batch
 g_pyaudio = None
 g_stream = None
 
-g_sndlog = b''
-g_consumed = 0
+g_buffer = b''
+
+# arbitrarily chosen maximum buffer length - in case the playback can't keep up
+g_buffer_max = 16384
+
+# to be adjusted if g_buffer_max is met
+g_freq = None
 
 
-def consume(in_data, frame_count, time_info, status):
-    global g_sndlog, g_consumed
+def _consume(in_data, frame_count, time_info, status):
+    global g_buffer
     size = frame_count * 2 * 2
-    while len(g_sndlog) < g_consumed + size:
-        time.sleep(0.01)
-    data = g_sndlog[g_consumed:g_consumed+size]
-    g_consumed += size
-    if g_consumed > 2**23:
-        g_consumed = 0
-        g_sndlog = ''
+    while len(g_buffer) < size:
+        time.sleep(0)
+    data, g_buffer = g_buffer[:size], g_buffer[size:]
     return data, pyaudio.paContinue
 
 
+def _transpose_frequency():
+    """
+    re-initialize the stream, transposed up by a fortieth of a (musical) step such that the consumer
+    might keep up with the sample producer by running a bit faster. yes, this is a bit of a hack.
+    """
+    global g_pyaudio, g_stream, g_buffer, g_freq
+    g_freq = int(g_freq * 2 ** (1/12 / 20))
+    g_buffer = b''
+    print('buffer overrun, adjusting frequency to', g_freq, 'Hz')
+    g_stream.close()
+    g_stream = g_pyaudio.open(format=pyaudio.paInt16, channels=2, rate=g_freq, output=True,
+                              stream_callback=_consume)
+    g_stream.start_stream()
+
+
 def pyaudio_init(core):
-    global g_pyaudio, g_stream
+    global g_pyaudio, g_stream, g_freq
     if g_pyaudio is None:
-        freq = int(core.get_av_info()['sample_rate']) or 32040
+        g_freq = int(core.get_av_info()['sample_rate']) or 32040
         g_pyaudio = pyaudio.PyAudio()
-        g_stream = g_pyaudio.open(format=pyaudio.paInt16, channels=2, rate=freq, output=True, stream_callback=consume)
+        g_stream = g_pyaudio.open(format=pyaudio.paInt16, channels=2, rate=g_freq, output=True,
+                                  stream_callback=_consume)
         g_stream.start_stream()
 
 
 def set_audio_sample_cb(core):
     pyaudio_init(core)
-    sndstruct = struct.Struct('<hh')
+    stereo_struct = struct.Struct('<hh')
 
     def wrapper(left, right):
-        global g_sndlog
-        g_sndlog += sndstruct.pack(left, right)
+        global g_buffer, g_buffer_max
+        if len(g_buffer) < g_buffer_max:
+            g_buffer += stereo_struct.pack(left, right)
+        else:
+            _transpose_frequency()
 
     core.set_audio_sample_cb(wrapper)
 
@@ -53,8 +73,11 @@ def set_audio_sample_batch_cb(core):
     pyaudio_init(core)
 
     def wrapper(data, frames):
-        global g_stream, g_sndlog
-        g_sndlog += ctypes.string_at(data, frames*2)
+        global g_buffer, g_buffer_max
+        if len(g_buffer) < g_buffer_max:
+            g_buffer += ctypes.string_at(data, frames * 2 * 2)
+        else:
+            _transpose_frequency()
         return frames
 
     core.set_audio_sample_batch_cb(wrapper)
