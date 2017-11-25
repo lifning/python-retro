11093c34afe649a41b2464f58838fee8da0b1616
lif <lifning>
2017-11-25 14:41:40 -0800

    memmove pixels directly into _pixels_address instead of marshalling through a python bytestring

--- py_retro/pygame_video.py
+++ py_retro/pygame_video.py
@@ -34,8 +34,7 @@ def set_video_refresh_cb(core, callback):
         bytes_per_pixel = (bpp + 7) // 8
         convsurf = pygame.Surface((pitch // bytes_per_pixel, height), depth=bpp, masks=bitmasks)
         surf = convsurf.subsurface((0, 0, width, height))
-
-        convsurf.get_buffer().write(ctypes.string_at(data, pitch * height), 0)
+        ctypes.memmove(convsurf._pixels_address, data, pitch*height)
 
         callback(surf)
 
