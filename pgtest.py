6cf795296eee3b6cb5147df22963bcd497e316bb
liffy <lifning@protonmail.com>
2018-02-06 23:05:06 -0800

    Add display geometry update support to pgtest.py

--- pgtest.py
+++ pgtest.py
@@ -7,22 +7,26 @@ libpath, rompath = sys.argv[1:3]
 
 
 def main():
-    es = py_retro.core.EmulatedSystem(libpath)
-    es.load_game_normal(path=rompath)
+    emu = py_retro.core.EmulatedSystem(libpath)
+    emu.load_game_normal(path=rompath)
 
-    screen = py_retro.pygame_video.pygame_display_set_mode(es, False)
+    fps = emu.get_av_info().get('fps', 60)
+    screen = py_retro.pygame_video.pygame_display_set_mode(emu, False)
+    py_retro.pygame_video.set_video_refresh_surface(emu, screen)
 
-    py_retro.pygame_video.set_video_refresh_surface(es, screen)
-    py_retro.portaudio_audio.set_audio_sample_internal(es)
-    py_retro.pygame_input.set_input_poll_joystick(es)
+    py_retro.portaudio_audio.set_audio_sample_internal(emu)
+    py_retro.pygame_input.set_input_poll_joystick(emu)
 
     # run each frame until closed.
     running = True
-    fps = es.get_av_info()['fps'] or 60
     clock = pygame.time.Clock()
 
     while running:
-        es.run()
+        if emu.av_info_changed:
+            fps = emu.get_av_info().get('fps', 60)
+            screen = py_retro.pygame_video.pygame_display_set_mode(emu, False)
+            py_retro.pygame_video.set_video_refresh_surface(emu, screen)
+        emu.run()
         pygame.display.flip()
         clock.tick(fps)
         for event in pygame.event.get():
