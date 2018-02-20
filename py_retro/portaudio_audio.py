70b93ed83372124f2d31e8af4039ea5aac2bc778
liffy <lifning@protonmail.com>
2018-02-19 22:49:08 -0800

    Convert portaudio backend to new class structure

--- py_retro/portaudio_audio.py
+++ py_retro/portaudio_audio.py
@@ -7,87 +7,56 @@ import ctypes
 import struct
 import time
 
-from .retro_globals import HACK_need_audio_sample_batch
+from .core import EmulatedSystem
 
-g_pyaudio = None
-g_stream = None
-
-g_buffer = b''
+g_stereo_struct = struct.Struct('<hh')
 
 g_sizeof_int16 = 2
 g_stereo_channels = 2
 
-# arbitrarily chosen maximum buffer length - in case the playback can't keep up
-g_buffer_max = 16384
 
-# to be adjusted if g_buffer_max is met
-g_freq = None
+class PortaudioMixin(EmulatedSystem):
+    def __init__(self, libpath, **kw):
+        super().__init__(libpath, **kw)
+        self.__pyaudio = pyaudio.PyAudio()
+        self.__stream = None
+        self.__buffer = bytearray()
 
+    def _set_timing(self, fps, sample_rate):
+        super()._set_timing(fps, sample_rate)
+        self.__set_sample_rate(int(sample_rate))
+        return True
 
-def _consume(in_data, frame_count, time_info, status):
-    global g_buffer
-    size = frame_count * g_sizeof_int16 * g_stereo_channels
-    while len(g_buffer) < size:
-        time.sleep(0)
-    data, g_buffer = g_buffer[:size], g_buffer[size:]
-    return data, pyaudio.paContinue
+    def __set_sample_rate(self, sample_rate):
+        self._sample_rate = sample_rate
+        if self.__stream:
+            self.__stream.close()
+        self.__stream = self.__pyaudio.open(
+            format=pyaudio.paInt16,
+            channels=g_stereo_channels,
+            rate=self._sample_rate,
+            output=True,
+            stream_callback=self.__consume)
+        self.__stream.start_stream()
 
+    def __consume(self, in_data, frame_count, time_info, status):
+        size = frame_count * g_sizeof_int16 * g_stereo_channels
+        data, self.__buffer = self.__buffer[:size], self.__buffer[size:]
+        if len(data) < size:
+            if self.trace:
+                print(f'portaudio underrun: {len(data)} < {size}')
+            data.extend([0] * (size - len(data)))
+        elif len(self.__buffer) > 2*size:
+            if self.trace:
+                print(f'portaudio overrun: {len(self.__buffer)} > {2*size}')
+            self.__buffer = self.__buffer[-size:]
+        return bytes(data), pyaudio.paContinue
 
-def _transpose_frequency():
-    """
-    re-initialize the stream, transposed up by a fortieth of a (musical) step such that the consumer
-    might keep up with the sample producer by running a bit faster. yes, this is a bit of a hack.
-    """
-    global g_pyaudio, g_stream, g_buffer, g_freq
-    g_freq = int(g_freq * 2 ** (1/12 / 20))
-    g_buffer = b''
-    print('buffer overrun, adjusting frequency to', g_freq, 'Hz')
-    g_stream.close()
-    g_stream = g_pyaudio.open(format=pyaudio.paInt16, channels=2, rate=g_freq, output=True,
-                              stream_callback=_consume)
-    g_stream.start_stream()
+    def _audio_sample(self, left, right):
+        sample = g_stereo_struct.pack(left, right)
+        self.__buffer.extend(sample)
 
-
-def pyaudio_init(core):
-    global g_pyaudio, g_stream, g_freq
-    if g_pyaudio is None:
-        g_freq = int(core.get_av_info()['sample_rate']) or 32040
-        g_pyaudio = pyaudio.PyAudio()
-        g_stream = g_pyaudio.open(format=pyaudio.paInt16, channels=g_stereo_channels, rate=g_freq,
-                                  output=True, stream_callback=_consume)
-        g_stream.start_stream()
-
-
-def set_audio_sample_cb(core):
-    pyaudio_init(core)
-    stereo_struct = struct.Struct('<hh')
-
-    def wrapper(left, right):
-        global g_buffer, g_buffer_max
-        if len(g_buffer) < g_buffer_max:
-            g_buffer += stereo_struct.pack(left, right)
-        else:
-            _transpose_frequency()
-
-    core.set_audio_sample_cb(wrapper)
-
-
-def set_audio_sample_batch_cb(core):
-    pyaudio_init(core)
-
-    def wrapper(data, frames):
-        global g_buffer, g_buffer_max
-        if len(g_buffer) < g_buffer_max:
-            g_buffer += ctypes.string_at(data, frames * g_sizeof_int16 * g_stereo_channels)
-        else:
-            _transpose_frequency()
+    def _audio_sample_batch(self, data, frames):
+        samples = ctypes.string_at(data, frames * g_sizeof_int16 * g_stereo_channels)
+        self.__buffer.extend(samples)
         return frames
-
-    core.set_audio_sample_batch_cb(wrapper)
-
-
-def set_audio_sample_internal(core):
-    if HACK_need_audio_sample_batch(core.name):
-        set_audio_sample_batch_cb(core)
-    else:
-        set_audio_sample_cb(core)
