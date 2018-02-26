c9c37efcef8f8903a086c15cafefbc1303609a38
lif <lifning@protonmail.com>
2018-02-25 22:36:31 -0800

    bugfixes in StatefulInput and FfmpegVideo

--- py_retro/tas/stateful_input.py
+++ py_retro/tas/stateful_input.py
@@ -8,12 +8,16 @@ def _recursive_defaultdict():
 
 
 class StatefulInputMixin(EmulatedSystem):
-    def __init__(self, libpath, **kw):
+    def __init__(self, libpath: str, **kw):
         super().__init__(libpath, **kw)
         self.__storage = _recursive_defaultdict()
 
     def _input_state(self, port: int, device: int, index: int, id_: int) -> int:
-        return self.__storage[port][device][index][id_]
+        val = self.__storage[port][device][index][id_]
+        if not isinstance(val, int):
+            self.set_input_state(port, device, index, id_, 0)
+            val = 0
+        return val
 
     def set_input_state(self, port: int, device: int, index: int, id_: int, state: int):
         self.__storage[port][device][index][id_] = state
