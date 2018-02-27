c2504495d538853e783e5a99941dfb6fa128c987
liffy <lifning@protonmail.com>
2018-02-26 21:28:12 -0800

    less hackish defaultdict for stateful_input

--- py_retro/tas/stateful_input.py
+++ py_retro/tas/stateful_input.py
@@ -3,20 +3,19 @@ import collections
 from ..core import EmulatedSystem
 
 
-def _recursive_defaultdict():
-    return collections.defaultdict(_recursive_defaultdict)
+def _recursive_defaultdict(depth, last_factory):
+    factory = (last_factory if depth <= 1
+               else lambda: _recursive_defaultdict(depth - 1, last_factory))
+    return collections.defaultdict(factory)
 
 
 class StatefulInputMixin(EmulatedSystem):
     def __init__(self, libpath: str, **kw):
         super().__init__(libpath, **kw)
-        self.__storage = _recursive_defaultdict()
+        self.__storage = _recursive_defaultdict(4, int)
 
     def _input_state(self, port: int, device: int, index: int, id_: int) -> int:
         val = self.__storage[port][device][index][id_]
-        if not isinstance(val, int):
-            self.set_input_state(port, device, index, id_, 0)
-            val = 0
         return val
 
     def set_input_state(self, port: int, device: int, index: int, id_: int, state: int):
